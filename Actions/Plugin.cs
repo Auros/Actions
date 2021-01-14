fcbfa20e31fc20f488ba1c57d4cc212478b8908c
Auros Nexus <aurosnex@gmail.com>
2021-01-13 21:11:51 -0500

    Initial commit

---
+++ Actions/Plugin.cs
@@ -0,0 +1,25 @@
+﻿using IPA;
+using SiraUtil;
+using SiraUtil.Zenject;
+using IPALogger = IPA.Logging.Logger;
+
+namespace Actions
+{
+    [Plugin(RuntimeOptions.DynamicInit)]
+    public class Plugin
+    {
+        [Init]
+        public Plugin(IPALogger log, Zenjector zenjector)
+        {
+            zenjector
+            .On<PCAppInit>()
+            .Pseudo(Container =>
+            {
+                Container.BindLoggerAsSiraLogger(log);
+            });
+        }
+
+        [OnEnable, OnDisable]
+        public void OnState() { /* On State */ }
+    }
+}
