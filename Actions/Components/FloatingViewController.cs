8b6e41ad0ae5370eace29c5bdda78f7bc266ccd7
Auros Nexus <aurosnex@gmail.com>
2021-01-14 15:27:07 -0500

    Create FloatingViewController.cs

---
+++ Actions/Components/FloatingViewController.cs
@@ -0,0 +1,90 @@
+﻿using HMUI;
+using Zenject;
+using UnityEngine;
+using VRUIControls;
+using IPA.Utilities;
+using BeatSaberMarkupLanguage.FloatingScreen;
+using BeatSaberMarkupLanguage.ViewControllers;
+
+namespace Actions.Components
+{
+    internal abstract class FloatingViewController<T> : BSMLAutomaticViewController where T : BSMLAutomaticViewController
+    {
+        private static readonly FieldAccessor<VRGraphicRaycaster, PhysicsRaycasterWithCache>.Accessor PhysicsRaycaster = FieldAccessor<VRGraphicRaycaster, PhysicsRaycasterWithCache>.GetAccessor("_physicsRaycaster");
+
+        private FloatingScreen? _floatingScreen;
+        private CurvedCanvasSettings? _curvedCanvasSettings;
+
+        #region Properties
+
+        private Vector2 _size = new Vector2(100f, 100f);
+        internal Vector2 Size
+        {
+            get => _size;
+            set
+            {
+                _size = value;
+                if (_floatingScreen != null)
+                {
+                    _floatingScreen.ScreenSize = _size;
+                }
+            }
+        }
+
+        private Vector3 _position = new Vector3(0f, 3.5f, 3f);
+        internal Vector3 Position
+        {
+            get => _position;
+            set
+            {
+                _position = value;
+                if (_floatingScreen != null)
+                {
+                    _floatingScreen.ScreenPosition = _position;
+                }
+            }
+        }
+
+        private Quaternion _rotation = Quaternion.identity;
+        internal Quaternion Rotation
+        {
+            get => _rotation;
+            set
+            {
+                _rotation = value;
+                if (_floatingScreen != null)
+                {
+                    _floatingScreen.ScreenRotation = _rotation;
+                }
+            }
+        }
+
+        private float _curveRadius = 0f;
+        internal float CurveRadius
+        {
+            get => _curveRadius;
+            set
+            {
+                _curveRadius = value;
+                if (_curvedCanvasSettings != null)
+                {
+                    _curvedCanvasSettings.SetRadius(_curveRadius);
+                }
+            }
+        }
+
+        #endregion
+
+        [Inject]
+        protected void Construct(PhysicsRaycasterWithCache physicsRaycasterWithCache)
+        {
+            _floatingScreen = FloatingScreen.CreateFloatingScreen(Size, false, Position, Rotation, CurveRadius);
+            _curvedCanvasSettings = GetComponentInChildren<CurvedCanvasSettings>(true);
+            var graphicRaycaster = _floatingScreen.GetComponent<VRGraphicRaycaster>();
+            PhysicsRaycaster(ref graphicRaycaster) = physicsRaycasterWithCache;
+            _floatingScreen.SetRootViewController(this, AnimationType.None);
+            _floatingScreen.name = $"{typeof(T).Name}Screen";
+            name = $"{typeof(T).Name}View";
+        }
+    }
+}
